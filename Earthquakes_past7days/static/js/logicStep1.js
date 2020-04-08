d208f2d9d668f056f32f925038ba63305b3711d8
Maria Carter <59865245+mcarter-00@users.noreply.github.com>
2020-04-08 15:40:10 -0700

    Adding files for Mapping Earthquake Data.

---
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,52 @@
+console.log('Is it working?!');
+
+/*
+ * Add Earthquake Data to a Map
+ */
+
+// Create a streets map layer.
+let streets = L.tileLayer(
+  'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: apiKey,
+  }
+);
+
+// Create a satelitte streets map layer.
+let satelliteStreets = L.tileLayer(
+  'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: apiKey,
+  }
+);
+
+// Create a base layer that holds both map layers.
+let baseMaps = {
+  Streets: streets,
+  'Satellite Streets': satelliteStreets,
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets],
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Retrieve the earthquake GeoJSON data.
+d3.json(
+  'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+).then(function (data) {
+  console.log(data);
+  // Create a GeoJSON layer with the retrieved data
+  L.geoJson(data).addTo(map);
+});
